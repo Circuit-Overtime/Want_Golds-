49a4d23de5569c41e21aec4ee0880c19551f0ed8
Circuit-Overtime <74301576+Circuit-Overtime@users.noreply.github.com>
2021-07-16 17:23:03 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,31 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+    // add code for displaying rest of the names and the hints
+
+
+    fill("white")
+    textSize(15)
+    text("N C F T U I N O", 700,150)
+    fill("lightblue")
+    text("Hint: Performs a Specific Task!!", 700,170)
+    
+
+    fill("white")
+    textSize(15)
+    text("E D T A S A B A", 100,240)
+    fill("lightblue")
+    text("Hint:  Stores all Information!!", 100,270)
+
+
+    
+
+}
